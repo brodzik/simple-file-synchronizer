1b750f3adb352b8e853e578fae78fffadb9588e2
Adrian Brodzik <adrianbrodzyk@gmail.com>
2020-01-02 19:13:42 +0100

    Recursive file iteration and checksum experiments

--- src/main/java/com/brodzik/adrian/simplefilesynchronizer/ui/MainView.java
+++ src/main/java/com/brodzik/adrian/simplefilesynchronizer/ui/MainView.java
@@ -1,14 +1,55 @@
 package com.brodzik.adrian.simplefilesynchronizer.ui;
 
+import com.brodzik.adrian.simplefilesynchronizer.util.Checksum;
 import de.saxsys.mvvmfx.FxmlView;
+import de.saxsys.mvvmfx.InjectViewModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class MainView implements FxmlView<MainViewModel> {
     @FXML
-    public Button buttonTest;
+    private Button buttonTest;
+
+    @Inject
+    private Stage primaryStage;
+
+    @InjectViewModel
+    private MainViewModel viewModel;
 
     public void initialize() {
-        buttonTest.setOnAction(actionEvent -> System.out.println("Click!"));
+        buttonTest.setOnAction(actionEvent -> {
+            System.out.println("Click!");
+
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            File selectedDirectory = directoryChooser.showDialog(primaryStage);
+
+            if (selectedDirectory == null) {
+                return;
+            }
+
+            System.out.println(selectedDirectory.getAbsolutePath());
+
+            try {
+                Files.walk(Paths.get(selectedDirectory.getAbsolutePath()))
+                        .filter(Files::isRegularFile)
+                        .forEach(path -> {
+                            try {
+                                File file = new File(path.toString());
+                                System.out.println(String.format("%s, %s", path, Checksum.getSHA256(file)));
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
---
+++ src/main/java/com/brodzik/adrian/simplefilesynchronizer/util/Checksum.java
@@ -0,0 +1,35 @@
+package com.brodzik.adrian.simplefilesynchronizer.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Checksum {
+    public static String getFileChecksum(File file, MessageDigest digest) throws IOException {
+        FileInputStream fis = new FileInputStream(file);
+
+        byte[] byteArray = new byte[1024];
+        int bytesCount = 0;
+
+        while ((bytesCount = fis.read(byteArray)) != -1) {
+            digest.update(byteArray, 0, bytesCount);
+        }
+
+        fis.close();
+
+        byte[] bytes = digest.digest();
+
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
+    }
+
+    public static String getSHA256(File file) throws NoSuchAlgorithmException, IOException {
+        return Checksum.getFileChecksum(file, MessageDigest.getInstance("SHA-256"));
+    }
+}
