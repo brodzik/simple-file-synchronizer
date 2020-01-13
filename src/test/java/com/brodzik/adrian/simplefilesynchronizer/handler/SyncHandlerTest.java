8b4a755390069cf7fa4cb9aa46d5b0b4bde0c200
Adrian Brodzik <adrianbrodzyk@gmail.com>
2020-01-14 00:52:19 +0100

    Cover other sync methods

--- src/test/java/com/brodzik/adrian/simplefilesynchronizer/handler/SyncHandlerTest.java
+++ src/test/java/com/brodzik/adrian/simplefilesynchronizer/handler/SyncHandlerTest.java
@@ -7,6 +7,8 @@ import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -18,7 +20,60 @@ import java.util.Date;
 
 class SyncHandlerTest {
     @Test
-    void testSync() throws IOException {
+    void testSyncUnidirectional() throws IOException {
+        SyncHandler.INSTANCE.fileList.clear();
+
+        Path pathA = Files.createTempDirectory("a");
+        Path pathB = Files.createTempDirectory("b");
+
+        {
+            FileWriter writer = new FileWriter(Paths.get(pathA.toString(), "test1.txt").toFile());
+            writer.write("test 1");
+            writer.close();
+        }
+
+        {
+            FileWriter writer = new FileWriter(Paths.get(pathB.toString(), "test2.txt").toFile());
+            writer.write("test 2");
+            writer.close();
+        }
+
+        SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.UNIDIRECTIONAL_1, 0, true, new Date(0)));
+        SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.UNIDIRECTIONAL_1, 0, true, new Date(0)));
+
+        Assertions.assertTrue(Paths.get(pathA.toString(), "test1.txt").toFile().isFile());
+        Assertions.assertFalse(Paths.get(pathA.toString(), "test2.txt").toFile().isFile());
+        Assertions.assertTrue(Paths.get(pathB.toString(), "test1.txt").toFile().isFile());
+        Assertions.assertFalse(Paths.get(pathB.toString(), "test2.txt").toFile().isFile());
+
+        FileUtils.forceDelete(Paths.get(pathA.toString(), "test1.txt").toFile());
+
+        SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.UNIDIRECTIONAL_1, 0, true, new Date(0)));
+        SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.UNIDIRECTIONAL_1, 0, true, new Date(0)));
+
+        Assertions.assertFalse(Paths.get(pathA.toString(), "test1.txt").toFile().isFile());
+        Assertions.assertFalse(Paths.get(pathA.toString(), "test2.txt").toFile().isFile());
+        Assertions.assertFalse(Paths.get(pathB.toString(), "test1.txt").toFile().isFile());
+        Assertions.assertFalse(Paths.get(pathB.toString(), "test2.txt").toFile().isFile());
+
+        {
+            FileWriter writer = new FileWriter(Paths.get(pathB.toString(), "test3.txt").toFile());
+            writer.write("test 3");
+            writer.close();
+        }
+
+        SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.UNIDIRECTIONAL_2, 0, true, new Date(0)));
+        SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.UNIDIRECTIONAL_2, 0, true, new Date(0)));
+
+        Assertions.assertTrue(Paths.get(pathA.toString(), "test3.txt").toFile().isFile());
+        Assertions.assertTrue(Paths.get(pathB.toString(), "test3.txt").toFile().isFile());
+
+        FileUtils.deleteDirectory(pathA.toFile());
+        FileUtils.deleteDirectory(pathB.toFile());
+    }
+
+    @Test
+    void testSyncBidirectional() throws IOException {
         SyncHandler.INSTANCE.fileList.clear();
 
         Path pathA = Files.createTempDirectory("a");
@@ -37,6 +92,7 @@ class SyncHandlerTest {
         }
 
         SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.BIDIRECTIONAL, 0, true, new Date(0)));
+        SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.BIDIRECTIONAL, 0, true, new Date(0)));
 
         Assertions.assertTrue(Paths.get(pathA.toString(), "test2.txt").toFile().isFile());
         Assertions.assertTrue(Paths.get(pathB.toString(), "test1.txt").toFile().isFile());
@@ -45,11 +101,88 @@ class SyncHandlerTest {
         FileUtils.forceDelete(Paths.get(pathA.toString(), "test2.txt").toFile());
 
         SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.BIDIRECTIONAL, 0, true, new Date(0)));
+        SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.BIDIRECTIONAL, 0, true, new Date(0)));
 
         Assertions.assertFalse(Paths.get(pathA.toString(), "test1.txt").toFile().isFile());
         Assertions.assertFalse(Paths.get(pathA.toString(), "test2.txt").toFile().isFile());
         Assertions.assertFalse(Paths.get(pathB.toString(), "test1.txt").toFile().isFile());
         Assertions.assertFalse(Paths.get(pathB.toString(), "test2.txt").toFile().isFile());
+
+        {
+            FileWriter writer = new FileWriter(Paths.get(pathA.toString(), "test1.txt").toFile());
+            writer.write("test 1");
+            writer.close();
+        }
+
+        {
+            FileWriter writer = new FileWriter(Paths.get(pathB.toString(), "test2.txt").toFile());
+            writer.write("test 2");
+            writer.close();
+        }
+
+        SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.BIDIRECTIONAL, 0, true, new Date(0)));
+
+        {
+            FileWriter writer = new FileWriter(Paths.get(pathB.toString(), "test2.txt").toFile());
+            writer.write("test 2 modified");
+            writer.close();
+        }
+
+        SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.BIDIRECTIONAL, 0, true, new Date(0)));
+
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(Paths.get(pathA.toString(), "test2.txt").toFile()));
+            Assertions.assertEquals("test 2 modified", reader.readLine());
+            reader.close();
+        }
+
+        {
+            FileWriter writer = new FileWriter(Paths.get(pathA.toString(), "test1.txt").toFile());
+            writer.write("test 1");
+            writer.close();
+        }
+
+        {
+            FileWriter writer = new FileWriter(Paths.get(pathB.toString(), "test2.txt").toFile());
+            writer.write("test 2");
+            writer.close();
+        }
+
+        SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.BIDIRECTIONAL, 0, true, new Date(0)));
+
+        {
+            FileWriter writer = new FileWriter(Paths.get(pathA.toString(), "test2.txt").toFile());
+            writer.write("test 2 modified");
+            writer.close();
+        }
+
+        SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.BIDIRECTIONAL, 0, true, new Date(0)));
+
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(Paths.get(pathB.toString(), "test2.txt").toFile()));
+            Assertions.assertEquals("test 2 modified", reader.readLine());
+            reader.close();
+        }
+
+        {
+            FileWriter writer = new FileWriter(Paths.get(pathA.toString(), "test2.txt").toFile());
+            writer.write("test 2 modified now");
+            writer.close();
+        }
+
+        {
+            FileWriter writer = new FileWriter(Paths.get(pathB.toString(), "test2.txt").toFile());
+            writer.write("test 2 modified later");
+            writer.close();
+        }
+
+        SyncHandler.INSTANCE.sync(new Entry(0, "", pathA.toString(), pathB.toString(), SyncDirection.BIDIRECTIONAL, 0, true, new Date(1)));
+
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(Paths.get(pathA.toString(), "test2.txt").toFile()));
+            Assertions.assertEquals("test 2 modified later", reader.readLine());
+            reader.close();
+        }
 
         FileUtils.deleteDirectory(pathA.toFile());
         FileUtils.deleteDirectory(pathB.toFile());
